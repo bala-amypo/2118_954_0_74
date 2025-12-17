ccf233f96d0c35a414c4dac824489e4a30f2b5a6
bala-amypo <bala@amypo.in>
2025-12-17 09:01:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studctl.java
+++ src/main/java/com/example/demo/controller/Studctl.java
@@ -1,30 +1,39 @@
 package com.example.demo.controller;
+
 import java.util.Collection;
-import org.springframework.beans.factory.annotation.AutoWired;
-import org.springframework.web.bind.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import com.example.demo.entity.Studentity;
 import com.example.demo.service.Studservice;
+
 @RestController
 @RequestMapping("/student")
-public class Studctl{
-    @AutoWired
+public class Studctl {
+
+    @Autowired
     private Studservice ser;
+
     @PostMapping("/add")
-    public Studentity addStudent(@RequestBody Studentity st){
+    public Studentity addStudent(@RequestBody Studentity st) {
         return ser.saveData(st);
     }
+
     @GetMapping("/getall")
-    public Collection<Studentity>getAllStudents(){
+    public Collection<Studentity> getAllStudents() {
         return ser.getAll();
     }
+
     @GetMapping("/get/{id}")
-    public Studentity getStudentById(@PathVariable int id){
+    public Studentity getStudentById(@PathVariable int id) {
         return ser.getById(id);
     }
+
     @PutMapping("/update/{id}")
     public Studentity updateStudent(
-        @PathVariable int id;
-        @RequestBody Studentity st){
-            return ser.update(id,st);
-        }
+            @PathVariable int id,
+            @RequestBody Studentity st) {
+        return ser.update(id, st);
+    }
 }
