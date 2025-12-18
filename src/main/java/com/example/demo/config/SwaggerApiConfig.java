2fc38920604f1d40e4f3af4e4368a363243c4bb7
bala-amypo <bala@amypo.in>
2025-12-18 06:26:44 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerApiConfig.java
+++ src/main/java/com/example/demo/config/SwaggerApiConfig.java
@@ -1,19 +1,28 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://9330.pro604cr.amypo.ai");
+        server.setDescription("transport_pro server");
+
         return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9242.pro604cr.amypo.ai/") // matches server.port
-                ));
+                .info(new Info()
+                        .title("Transport Pro API")
+                        .version("1.0.0")
+                        .description("Spring Boot APIs for transport_pro project"))
+                .servers(List.of(server));
     }
 }
