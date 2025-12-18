523f1d0257257fbb70e04b91a0878c87c8ecc2fc
bala-amypo <bala@amypo.in>
2025-12-18 06:09:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/Userimpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.Impl;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Userentity;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepo;
+import com.example.demo.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserServiceImpl(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Userentity register(Userentity user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        if (user.getRole() == null || user.getRole().isEmpty()) {
+            user.setRole("USER");
+        }
+
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Userentity findByEmail(String email) {
+        return userRepo.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
+    }
+}
